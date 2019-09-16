a98fafa6a0c28d915de1b272d0181dc6594fa4b8
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-16 20:57:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       @receiver.balance += @amount 
       @status = "complete"
     else
-      return "Transaction rejected. Please check your account balance."
+      "Transaction rejected. Please check your account balance."
     end 
     
     # unless @status == "complete" 
