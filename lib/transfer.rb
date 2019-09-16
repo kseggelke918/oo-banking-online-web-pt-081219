746ee7e6d9755d157e0af08eaa2814d51ac10060
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-16 21:04:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,7 +26,10 @@ class Transfer
   end 
   
   def reverse_transfer
-      
+    if @status == "complete" && self.valid?
+      @receiver.balance -= @amount
+      @sender.balance += @amount 
+    end 
   end 
   
 end
